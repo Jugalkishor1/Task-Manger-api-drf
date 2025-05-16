46bbd0b24d2db9fd31b383d3162d7fc30ff35323
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-16 23:39:03 +0530

    Change our db to postgres

--- task/settings.py
+++ task/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jugal_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
