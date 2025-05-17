67c0378d48ca20d5501ea7cec88a04080682c2c5
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-17 13:00:56 +0530

    Create task serializer

---
+++ tasklist/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Tasklist
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tasklist
+        fields = ['id', 'title', 'description', 'status', 'user']
