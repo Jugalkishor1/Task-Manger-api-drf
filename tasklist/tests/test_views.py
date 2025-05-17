791d01f7705451310dc58821c9e11b2a418eb719
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-17 22:10:05 +0530

    Test cases for task list and create api

---
+++ tasklist/tests/test_views.py
@@ -0,0 +1,50 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from ..models import Tasklist
+import pdb
+
+User = get_user_model()
+
+class TaskListViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='test@example.com', password='testpass123')
+        self.token = RefreshToken.for_user(self.user).access_token
+        self.auth_header = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
+        self.url = reverse('task-list')
+
+    def test_list_all_users_tasks(self):
+        task1 = Tasklist.objects.create(user=self.user, title='Task 1')
+
+        response = self.client.get(self.url, **self.auth_header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['title'], task1.title)
+
+
+    def test_get_tasks_unauthenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_post_task_success(self):
+        data = {
+            "title": "Test Task",
+            "description": "Test task description",
+            "status": "not_started"
+        }
+
+        response = self.client.post(self.url, data, format='json', **self.auth_header)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], data['title'])
+
+    def test_post_task_with_invlaid_attr(self):
+        data = {
+            "title": "",
+            "status": "not_started"
+        }
+
+        response = self.client.post(self.url, data, format='json', **self.auth_header)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('title', response.data)
