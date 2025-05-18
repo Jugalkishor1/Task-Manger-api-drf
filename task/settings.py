70eccc28abf441341146526e9b7501f8bc952aa1
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-18 21:25:07 +0530

    add configuration to serve static file

--- task/settings.py
+++ task/settings.py
@@ -15,9 +15,10 @@ from datetime import timedelta
 import os
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'task.urls'
 
@@ -170,6 +174,3 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'task-manger-api-1cq2.onrender.com'),
 ]
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
